01e8f47baa16b2dc2ec0abcf5ea1e0ec88a8bfeb
Handerson Lucena <160972288+HandersonLuc@users.noreply.github.com>
2025-02-28 15:13:11 -0300

    Feat: add 11° exercice

---
+++ Lista_0/Exercício_11.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    float salary, minimun_wage, relacao;
+    printf("Digite o salario: \n");
+    scanf("%f", &salary);
+    printf("digite o salario minimo: \n");
+    scanf("%f", &minimun_wage);
+    relacao = salary/minimun_wage;
+    printf("O salario e %.2f vezes o salario minimo\n", relacao);
+    return 0;
+}
