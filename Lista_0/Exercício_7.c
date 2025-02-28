6e1039104c99e20f50ab22aad2ce8b8e7da04e71
Handerson Lucena <160972288+HandersonLuc@users.noreply.github.com>
2025-02-28 15:11:11 -0300

    Feat: add 7° exercice

---
+++ Lista_0/Exercício_7.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    float nota_1, nota_2, nota_3;
+    printf("Nota 1: ");
+    scanf("%f", &nota_1);
+    printf("Nota 2: ");
+    scanf("%f", &nota_2);
+    printf("Nota 3: ");
+    scanf("%f", &nota_3);
+    float media = (nota_1 + nota_2 + nota_3) / 3;
+    printf("A media das notas e %.2f\n", media);
+    return 0;
+}
